2ad597ff5f4ff1d406acfb25433abcadda448196
Todd Wolfson <todd@twolfson.com>
2013-04-27 16:44:23 -0700

    Thinking more about handling content and any other abstractions

--- test/phantomjssmith_test_outline.js
+++ test/phantomjssmith_test_outline.js
@@ -1,3 +1,5 @@
+// TODO: We could use inversion of control for a common testing module.
+// This would allow for common test images, content, and isolation of those images to a `devDependency`
 {
   'phantomjssmith': {
     'interpretting an image file': {
