7c02c8e7af208cf47a0d59acd7f43637df6b183d
Todd Wolfson <todd@twolfson.com>
2013-04-27 20:02:33 -0700

    Successfully asserting image stats

--- test/phantomjssmith_test_content.js
+++ test/phantomjssmith_test_content.js
@@ -1,14 +1,22 @@
 // TODO: We could use inversion of control for a common testing module.
 // This would allow for common test images, content, and isolation of those images to a `devDependency`
-var smith = require('../lib/phantomjssmith');
+var expect = require('chai').expect,
+    smith = require('../lib/phantomjssmith');
 module.exports = {
   'phantomjssmith': function () {
+    // Do nothing
   },
-  'interpretting an image file': function () {
-    console.log(smith);
+  'interpretting an image file': function (done) {
+    // Create an image and save it for later
+    var filepath = __dirname + '/test_sprites/sprite1.png',
+        that = this;
+    smith.createImage(filepath, function (err, img) {
+      that.img = img;
+      done(err);
+    });
   },
   'gathers statistics on an image file':  function () {
-
+    expect(this.img).to.deep.equal({height: 50, width: 50});
   },
   'parsing many image files': function () {
 
