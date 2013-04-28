13035f80889fb2b3d0b0976c089cfb0a8b9997f7
Todd Wolfson <todd@twolfson.com>
2013-04-27 21:49:17 -0700

    Relocated test outline as well

--- test/phantomjssmith_test_outline.js
+++ test/phantomjssmith_test_outline.js
@@ -1,20 +1,5 @@
 // TODO: We could use inversion of control for a common testing module.
 // This would allow for common test images, content, and isolation of those images to a `devDependency`
 module.exports = {
-  'phantomjssmith': {
-    'interpretting an image file': {
-      'gathers statistics on an image file': true
-    },
-    'parsing multiple images': {
-      'rendering them into a canvas': {
-        'can output an image': true
-      }
-    },
-    'interpretting a ridiculous amount of images': {
-      'rendering them into a canvas': {
-        'does not crash': true,
-        'returns an image': true
-      }
-    }
-  }
+  'phantomjssmith': require('./common_testoutline')
 };
