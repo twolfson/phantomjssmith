09de34eb7811e55c2bd205d62dd25398bc0216d1
Todd Wolfson <todd@twolfson.com>
2013-09-22 19:04:08 -0700

    Attempting to use apt-get

--- test/setup.sh
+++ test/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # If we are installing PhantomJS@1.9.2
-if [[ "$PHANTOMJS_VERSION" == "1.9.2." ]]; then
-  echo "Installing 1.9.2"
+if [[ "$PHANTOMJS_VERSION" == "latest" ]]; then
+  sudo apt-get install phantomjs -y
 fi
