95c78b9e14387ad80c91bdf790c1750b9e7a1563
Todd Wolfson <todd@twolfson.com>
2013-09-22 19:11:40 -0700

    Installing phantomjs from website

--- test/install.sh
+++ test/install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 # If we are installing PhantomJS@1.9.2
-if [[ "$PHANTOMJS_VERSION" == "latest" ]]; then
-  sudo apt-get install phantomjs -y
+if [[ "$PHANTOMJS_VERSION" == "1.9.2." ]]; then
+  # Uninstall current version
+  sudo apt-get uninstall phantomjs -y
+
+  # Download and install 1.9.2 from website
+  wget "https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2"
+  tar xvf phantomjs-1.9.2-linux-x86_64.tar.bz2
+  sudo ln -s $PWD"/phantomjs-1.9.2-linux-x86_64/bin/phantomjs" /usr/bin/phantomjs
 fi
