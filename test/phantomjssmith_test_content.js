95c1836af523ab7c4d0e384d2014eb9ac631c855
Todd Wolfson <todd@twolfson.com>
2013-11-24 01:53:25 -0800

    Finding our injection point for the test

--- test/phantomjssmith_test_content.js
+++ test/phantomjssmith_test_content.js
@@ -8,7 +8,31 @@ module.exports = extend({}, commonTest, {
     var expectedDir = __dirname + '/expected_files/';
     this.expectedFilepaths = [expectedDir + '/multiple.png', expectedDir + '/multiple2.png'];
   },
-  'running against very long URLs': function () {
-    console.log('hi');
-  }
+  'running against very long URLs': ['parsing multiple images', function (done) {
+    var that = this,
+        smith = this.smith;
+    smith.createImages(this.images, function handleImages (err, imgs) {
+      // If there is an error, callback with it
+      if (err) { return done(err); }
+
+      // Otherwise, draw them onto a canvas
+      smith.createCanvas(that.width, that.height, function (err, canvas) {
+        // If there is an error, callback with it
+        if (err) { return done(err); }
+
+        // Add each image
+        var coordinatesArr = that.coordinateArr;
+        imgs.forEach(function (img, i) {
+          var coordinates = coordinatesArr[i];
+          canvas.addImage(img, coordinates.x, coordinates.y);
+        }, canvas);
+
+        // Export canvas
+        canvas['export']({format: 'png', looooong: 'infooo'}, function (err, result) {
+          that.result = result;
+          done(err);
+        });
+      });
+    });
+  }]
 });
