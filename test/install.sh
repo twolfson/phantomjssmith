ee0b9a055de4abe88e84ab7fd178617f0e6bdcff
Todd Wolfson <todd@twolfson.com>
2014-11-24 22:27:47 -0800

    Fixed up test installer

--- test/install.sh
+++ test/install.sh
@@ -8,8 +8,16 @@ if [[ "$PHANTOMJS_VERSION" != "TRAVIS" ]]; then
   # Uninstall current version
   sudo rm "$(which phantomjs)"
 
-  # Download and install our version from website
-  wget "https://phantomjs.googlecode.com/files/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
+  # Download our version from the internet
+  # If we are at a pre-bitbucket version, use Google code
+  if [[ "$PHANTOMJS_VERSION" = "1.9.2" ]]; then
+    wget "https://phantomjs.googlecode.com/files/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
+  # Otherwise, use bitbucket
+  else
+    wget "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
+  fi
+
+  # Extract and install PhantomJS
   tar xvf "phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
   sudo ln -s "$PWD/phantomjs-$PHANTOMJS_VERSION-linux-x86_64/bin/phantomjs" "/usr/bin/phantomjs"
 fi
