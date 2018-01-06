3d4a5e94d4f5293d17f7ce4e5abb579d10161b45
Todd Wolfson <todd@twolfson.com>
2018-01-06 02:17:22 -0800

    Simplified PhantomJS testing to 1.9.8 (Bitbucket) and 2.x.x (Travis CI)

--- test/install.sh
+++ test/install.sh
@@ -3,29 +3,13 @@
 set -e
 set -x
 
-# If we are installing a non-official binary
-if [[ "$PHANTOMJS_VERSION" = "2.0.0" ]]; then
-  # Uninstall current version
-  sudo rm "$(which phantomjs)"
-
-  # Download Travis CI's version
-  wget "https://s3.amazonaws.com/travis-phantomjs/phantomjs-$PHANTOMJS_VERSION-ubuntu-12.04.tar.bz2"
-
-  tar xvf "phantomjs-$PHANTOMJS_VERSION-ubuntu-12.04.tar.bz2"
-  sudo ln -s "$PWD/phantomjs" "/usr/bin/phantomjs"
-# Otherwise if we are installing not using the default Travis version
-elif [[ "$PHANTOMJS_VERSION" != "TRAVIS" ]]; then
+# If we are installing not using the default Travis version
+if [[ "$PHANTOMJS_VERSION" != "TRAVIS" ]]; then
   # Uninstall current version
   sudo rm "$(which phantomjs)"
 
   # Download our version from the internet
-  # If we are at a pre-bitbucket version, use Google code
-  if [[ "$PHANTOMJS_VERSION" = "1.9.2" ]]; then
-    wget "https://phantomjs.googlecode.com/files/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
-  # Otherwise, use bitbucket
-  else
-    wget "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
-  fi
+  wget "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
 
   # Extract and install PhantomJS
   tar xvf "phantomjs-$PHANTOMJS_VERSION-linux-x86_64.tar.bz2"
